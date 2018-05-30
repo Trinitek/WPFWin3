ee746b89f5cf505c9550a3d394b35bc08e265889
Blake Burgess <trinitek@live.com>
2018-05-30 05:37:27 -0400

    Remove gradient from ProgressBar's indeterminate mode

---
+++ Windows3Style/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Windows3Style.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : MarkupExtension, IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        #endregion
+    }
+}
--- Windows3Style/Converters/MathConverter.cs
+++ Windows3Style/Converters/MathConverter.cs
@@ -11,7 +11,7 @@ using System.Windows;
 using System.Windows.Data;
 using System.Windows.Markup;
 
-namespace IKriv.Wpf
+namespace Windows3Style.Converters
 {
     /// <summary>
     /// Value converter that performs arithmetic calculations over its argument(s)
