bacd86bd3ec864e580a83010cbe83d70c94e00d7
Blake Burgess <trinitek@live.com>
2019-02-09 05:20:45 -0500

    Add FontSizeExtension

---
+++ Windows3Style/Extensions/FontSizeExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace Windows3Style.Extensions
+{
+    public class FontSizeExtension : MarkupExtension
+    {
+        [TypeConverter(typeof(FontSizeConverter))]
+        public double Size { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => Size;
+    }
+}
